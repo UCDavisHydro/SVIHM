24eee5c33e8388d76d43526bb03efeafc8cc02b7
Leland Scantlebury <leland@scantle.com>
2022-08-08 20:44:27 -0700

    Expansion of Update_Starting_Heads to be able to average over many SPs

--- Utilities/Update_Starting_Heads/Update_Starting_Heads.f90
+++ Utilities/Update_Starting_Heads/Update_Starting_Heads.f90
@@ -8,19 +8,28 @@ program Update_Starting_Heads
 ! Reads in head solution at specified SP, writes to arrays
 ! Written to replace Update_SVIHM_Starting_heads R script
 !
-! Command line arguments: heads file name (e.g., SVIHM.hds)
+! New in v0.0.2 (8/8/2022, by LS)
+!  - Can set end SP, values will be averaged between start and end
+!
+! Command line arguments:
+!  - heads file name (e.g., SVIHM.hds)
+!  - (optional) desired stress period to get heads from
+!  - (optional) end stress period to get heads from - heads will be averaged from start to end sp
+!    (inclusive on both sides)
+!
 ! Output: Files in the form of 'Starting_Heads_L*.txt' where * is the layer
 !-------------------------------------------------------------------------------------------------!
   
-  integer              :: i, j, k, ierr, kstp, kper, ncol, nrow, ilay, target_sp
+  integer              :: i, j, k, ierr, kstp, kper, ncol, nrow, nsp, nlay, ilay, target_sp, end_sp
+  integer              :: get_nlay
   real                 :: pertim, totim
-  real,allocatable     :: heads(:,:)
+  real,allocatable     :: heads(:,:,:), temp_heads(:,:,:)
   character(len=16)    :: text, fmt
   character(len=60)    :: hds_file, outname
   
   write(*,*) '********************************************'
   write(*,*) '* Update_Starting_Heads - an SVIHM Utility *'
-  write(*,*) '* v0.0.1                                   *'
+  write(*,*) '* v0.0.2                                   *'
   write(*,*) '********************************************'
   write(*,*) 'Status:'
   
@@ -42,8 +51,24 @@ program Update_Starting_Heads
     ! Assume first SP is wanted
     target_sp = 1
   end if
+
+  ! If passed, get END sp
+  if (COMMAND_ARGUMENT_COUNT() > 2) then
+    call getarg(3,text)
+    read(text, '(i)') end_sp
+    nsp = end_sp - target_sp + 1
+  else
+    end_sp = target_sp
+    nsp = 1
+  end if
   
-  write(*,'(3x,a,i3)') 'Arrays will be from last entry from SP:', target_sp
+  if (nsp == 1) then
+    write(*,'(3x,a,i3)') 'Arrays will be from last entry from SP:', target_sp
+  else
+    write(*,'(3x,3(a,i3))') 'Arrays will be the average of SPs:', target_sp, '-', end_sp
+  end if
+
+  nlay = get_nlay(hds_file)
   
   ! Open heads file
   write(*,'(3x,2a)') 'Reading Heads File: ', trim(hds_file)
@@ -53,33 +78,44 @@ program Update_Starting_Heads
   read(10,iostat=ierr) kstp, kper, pertim, totim, text, ncol, nrow, ilay
   write(*,'(3x,2(a,i4,2x))') 'Model Discretization | Rows: ', nrow, 'Columns: ', ncol
   
-  ! Allocate
-  allocate(heads(ncol, nrow))
+  ! Allocate, Initialize
+  allocate(temp_heads(ncol, nrow, nlay), heads(ncol, nrow, nlay))
+  heads = 0.0
   
-  ! Read & Write
+  ! Get to it
   write(*,*) 'Reading:'
   k = 1
   do while (ierr == 0)
+    ! Read 
     if (k>1) then
       read(10,iostat=ierr) kstp, kper, pertim, totim, text, ncol, nrow, ilay
     end if
-        ! If we've passed the target sp (all layers hopefully written) then exit
-    if (kper > target_sp) then
-      exit
-    end if
     ! Report
     write(6,'("+",3x,3(a,i4,2x))') 'Time Step', kstp, 'Stress Period', kper, 'Layer', ilay
-    read(10,iostat=ierr) heads(1:ncol, 1:nrow)
-    ! If it's the target sp, write it out
-    if (kper == target_sp) then
-      write(outname,'("Starting_Heads_L",i1,".txt")') ilay
-      write(fmt,'(a1,i3,"es16.8e2",a1)') "(", ncol, ")"  ! Nearly certian there's a smarter way...
-      write(*,*) 'Writing file ', outname
-      open(20, file=outname, status='replace', action='write', iostat=ierr)
-      write(20,fmt) heads
-      close(20)
+    ! If we've passed the target sp (all layers hopefully read) then exit
+    if (kper > target_sp) then
+      heads = heads + temp_heads
+      target_sp = target_sp + 1
     end if
-    k = k +1
+    read(10,iostat=ierr) temp_heads(1:ncol, 1:nrow, ilay)
+    k = k + 1
+    ! Second exit condition
+    if (target_sp > end_sp) exit
+  end do
+
+  ! Average
+  if (nsp > 1) then
+    heads = heads / nsp
+  end if
+
+  ! Write
+  do ilay=1, nlay
+    write(outname,'("Starting_Heads_L",i1,".txt")') ilay
+    write(fmt,'(a1,i3,"es16.8e2",a1)') "(", ncol, ")"
+    write(*,*) 'Writing file ', outname
+    open(20, file=outname, status='replace', action='write', iostat=ierr)
+    write(20,fmt) heads(:,:,ilay)
+    close(20)
   end do
   
   deallocate(heads)
@@ -89,3 +125,35 @@ program Update_Starting_Heads
   write(*,*) 'Done.'
   
 end program Update_Starting_Heads
+
+!-------------------------------------------------------------------------------------------------!
+
+integer function get_nlay(filepath) result(nlay)
+  implicit none
+
+  integer              :: kstp, kper, ncol, nrow, ilay, ierr=0
+  real                 :: pertim, totim
+  real,allocatable     :: heads(:,:)
+  character(len=16)    :: text, fmt
+  character(*),intent(in) :: filepath
+
+  ! Open file
+  open(10, file=trim(filepath), form='binary', status='old', action='read', iostat=ierr)
+
+  nlay = 0
+  do while (ierr == 0)
+    read(10,iostat=ierr) kstp, kper, pertim, totim, text, ncol, nrow, ilay
+    if (allocated(heads)) then
+      ! Nothing!
+    else
+      allocate(heads(ncol,nrow))
+    end if
+    if (ilay < nlay) exit  ! Not getting larger - nlay found
+    nlay = ilay            ! Otherwise this is our new max layer
+    read(10,iostat=ierr) heads(1:ncol, 1:nrow)
+  end do
+
+  close(10)
+
+  return
+end function get_nlay
