dbe98563897693bf25e31ded7040f01d8be49e0a
Leland Scantlebury <leland@scantle.com>
2024-05-04 13:46:44 -0700

    More notes for InstalLibraries after testing

--- Scripts/RSVP/00_InstallLibraries.R
+++ Scripts/RSVP/00_InstallLibraries.R
@@ -2,6 +2,9 @@
 #-- Most importantly, it installs RMODLFOW from GitHub
 #-- All other packages are available on CRAN
 
+# Note - please install Rtools first:
+# https://cran.rstudio.com/bin/windows/Rtools/
+
 # Setup -------------------------------------------------------------------
 
 # List of CRAN packages to be installed
@@ -31,3 +34,9 @@ if(length(new.packages)) install.packages(new.packages)
 #remotes::install_github("rogiersbart/RMODFLOW")
 devtools::install_github("ropensci/rnoaa")
 devtools::install_github("cneyens/RMODFLOW@develop")
+
+# Assuming that all worked, you should now be able to build the RSVP Package
+# Build -> Install Package (Ctrl+Shift+B on Windows)
+
+# Next you should be able to run 01_InputDataUpdate & 02_BaseUpdate
+# Or any of the other scripts requiring RSVP
